c402ef9675bd66582b2e36c1e8a9e236b88aca31
Marcelo Augusto <marceloaugustobra@gmail.com>
2025-11-24 10:21:08 -0300

    Categoria e Atividade

---
+++ src/main/java/com/MarceloAugustoBra/modelo_de_dominio_ORM/entities/Atividade.java
@@ -0,0 +1,62 @@
+package com.MarceloAugustoBra.modelo_de_dominio_ORM.entities;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String descricao;
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Atividade(){}
+
+    public Atividade(Integer id, String name, String descricao, Double price) {
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
