fe95749677848aa69df7edd2bed80e1f17a82d03
Marcelo Augusto <marceloaugustobra@gmail.com>
2025-11-24 09:29:55 -0300

    Primeiro Commit

---
+++ src/main/java/com/MarceloAugustoBra/modelo_de_dominio_ORM/ModeloDeDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.MarceloAugustoBra.modelo_de_dominio_ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloDeDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloDeDominioOrmApplication.class, args);
+	}
+
+}
